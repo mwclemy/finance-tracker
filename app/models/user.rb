94de8665d9a0bb120926c637323e89291baf46d0
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-12-30 23:00:08 -0700

    implemented stock tracking restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def can_track_stock(ticker_symbol)
+    !is_tracking_stock(ticker_symbol) && !reached_stocks_limit
+  end 
+
+  def is_tracking_stock(ticker_symbol)
+    stock = Stock.search_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id:stock.id).exists?
+  end
+
+  def reached_stocks_limit
+      stocks.count >= 10
+  end
 end
