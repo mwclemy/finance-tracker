1595d44ee4c3d4854b68f9665ffd6ba5ed83dfb3
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2022-12-30 17:57:44 -0700

    added stock tracking functionanlity

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,17 @@
 class UserStocksController < ApplicationController
+    def create 
+        stock = Stock.search_db(params[:ticker])
+        if stock.blank?
+            stock = Stock.new_lookup(params[:ticker])
+            stock.save
+        end
+        @user_stock = UserStock.create(user: current_user, stock: stock)
+        if @user_stock
+            flash[:notice] = "#{stock.name} stock has been successfully added to your portfolio"
+            redirect_to my_portfolio_path
+        else
+            flash[:alert] = "Something went wrong. Please try again."
+            redirect_to my_portfolio_path
+        end
+    end
 end
